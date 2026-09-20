ae68bb7a50368882abd35a419ebfbc0ba7faa7d2
robloxdevcode <ltrobloxbrothers@gmail.com>
2026-09-20 16:32:36 +0300

    .

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import { HomeLanding } from "@/components/marketing/home-landing";
-import {
-  CtaSection,
-  FaqSection,
-  HowItWorksSection,
-  PlatformMarquee,
-  PlansSection,
-  UseCasesSection,
-} from "@/components/marketing/home-sections";
+import { HowItWorksSection, PlansSection } from "@/components/marketing/home-sections";
 import { HomeStructuredData } from "@/components/marketing/home-structured-data";
 import { buildPageMetadata, HOME_KEYWORDS, HOME_META_DESCRIPTION } from "@/lib/seo";
 
@@ -23,12 +16,8 @@ export default async function HomePage() {
     <>
       <HomeStructuredData />
       <HomeLanding />
-      <PlatformMarquee />
       <HowItWorksSection />
-      <UseCasesSection />
       <PlansSection />
-      <FaqSection />
-      <CtaSection />
     </>
   );
 }
